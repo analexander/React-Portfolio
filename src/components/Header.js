bbe8a2cb15065de496d539b292fe6cf7cde80adb
Angel Alexander <angale22@gmail.com>
2020-11-22 14:35:59 -0600

    adding navbvbar and background img

---
+++ src/components/Header.js
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+} from 'reactstrap';
+
+const Example = (props) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => setIsOpen(!isOpen);
+
+  return (
+    <div>
+      <Navbar color="dark" light expand="md">
+        <NavbarBrand href="/">Angel Alexander</NavbarBrand>
+        <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav className="mr-auto" navbar>
+            <NavItem>
+              <NavLink href="/portfolio/">Portfolio</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="https://github.com/analexander" target="_blank">GitHub</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="https://www.linkedin.com/in/angel-alexander-1171571a5/" target="_blank">LinkedIn</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="https://docs.google.com/document/d/e/2PACX-1vRIDbxm8dCUo8dCUXuqeNqHuIv-RgfXakkY2m0QcuETLT1fyBXTWUoihEOijebva6fRYUcEJVCUPfjp/pub" target="_blank">Resume</NavLink>
+            </NavItem>
+          </Nav>
+        </Collapse>
+      </Navbar>
+    </div>
+  );
+}
+
+export default Example;
