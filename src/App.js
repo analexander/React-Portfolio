2ff06c6879d41dccfc257e5ac6aeccdcdd1459ab
Angel Alexander <angale22@gmail.com>
2020-11-24 14:37:43 -0600

    img renderr portfolio

--- src/App.js
+++ src/App.js
@@ -6,16 +6,19 @@ import projectData from './projectData';
 import "./App.css";
 
 function App() {
+  // {projectData.map(props => {
+  //   return <img key={props.id} src={props.img} alt="can't show image" />;
+  // })}
     const projectObj = projectData.map(project => <Project key={project.id} item={project}/>)
   return (
     <div className="App">
       <Header />
-      <Project project={projectObj[0].props.item}/>
-      <Project project={projectObj[1].props.item}/>
-      <Project project={projectObj[2].props.item}/>
-      <Project project={projectObj[3].props.item}/>
-      <Project project={projectObj[4].props.item}/>
-      <Project project={projectObj[5].props.item}/>
+      <Project project={projectObj[0].props.item} src={projectObj[0].props.item.img}/>
+      <Project project={projectObj[1].props.item} src={projectObj[1].props.item.img}/>
+      <Project project={projectObj[2].props.item} src={projectObj[2].props.item.img}/>
+      <Project project={projectObj[3].props.item} src={projectObj[3].props.item.img}/>
+      <Project project={projectObj[4].props.item} src={projectObj[4].props.item.img}/>
+      <Project project={projectObj[5].props.item} src={projectObj[5].props.item.img}/>
       <Footer />
     </div>
   )
--- src/components/Project.js
+++ src/components/Project.js
@@ -1,4 +1,11 @@
 import React from 'react';
+import Habitude from '../Images/habit1 copy.png'
+import ReadmeGen from '../Images/READMEsample.png'
+import WorkDay from '../Images/work-day-scheduler.png'
+import NoteTaker from '../Images/note-taker.png'
+import WeatherDash from '../Images/weather-dashboard.png'
+import EmployeeTrack from '../Images/walkthrough copy.gif'
+
 import {
     CardDeck, CardText, CardBody, CardLink,
     CardTitle, CardSubtitle, Row, Col
@@ -12,7 +19,7 @@ const Project = props => {
                         <CardTitle tag="h5">{props.project.title}</CardTitle>
                         <CardSubtitle tag="h6" className="mb-2 text-muted">{props.project.subtitle}</CardSubtitle>
                     </CardBody>
-                    <img width="100%" src={props.project.img} alt="project image" />
+                    <img width="100%" src={Habitude} alt="project screenshot" />
                     <CardBody>
                         <CardText>{props.project.description}</CardText>
                         <CardLink href={props.project.github}>GitHub repo</CardLink>
