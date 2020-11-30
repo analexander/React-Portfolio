6056aab7c2c40e4f5b3c5233b261dd0600024578
Angel Alexander <angale22@gmail.com>
2020-11-30 14:03:04 -0600

    Updates

---
+++ precache-manifest.c4b327ffec1b67fe4b5204e1b6554487.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ef8d1bb5dc76b370babb",
+    "url": "/React-Portfolio/static/css/main.2c83de2b.chunk.css"
+  },
+  {
+    "revision": "ef8d1bb5dc76b370babb",
+    "url": "/React-Portfolio/static/js/main.ef8d1bb5.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/React-Portfolio/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "804d233a10add83efa8d",
+    "url": "/React-Portfolio/static/js/runtime~main.804d233a.js"
+  },
+  {
+    "revision": "65ec2d331826a947ddb0672bf6b79371",
+    "url": "/React-Portfolio/index.html"
+  }
+];
